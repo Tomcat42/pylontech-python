6821f7bbd41fc905d71a803f60c6b45c7088ec2c
Bernd Singer <singer@nefkom.net>
2022-06-03 20:52:32 +0200

    fix installation with setup.py
    the name oft the package 'pylon' did not fit the directory name.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 setup(
     name="pylontech",
     version="0.1.0",
-    packages=find_packages(include=['pylon', 'pylon.*']),
+    packages=find_packages(include=['pylontech', 'pylontech.*']),
     author="Bernd Singer",
     author_email="singer@nefkom.net",
     description=("Communicate to Pylontech batteries using a RS485 interface"),
     license="MIT",
-    keywords="pylontech pylon rs485 lithium battery US2000 US2000C US3000",
+    keywords="pylontech pylon RS485 lithium battery US2000 US2000C US3000",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
