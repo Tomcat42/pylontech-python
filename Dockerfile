fcd0c17a358390a469429b2d98d38b7f03720ff5
Daniel Schramm <daniel.schramm@gmx.de>
2023-05-24 22:45:45 +0200

    Config page with hints

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+ARG BUILD_FROM
+FROM $BUILD_FROM
+
+# Install requirements for add-on
+RUN \
+  apk add --no-cache \
+    python3 py3-pip
+
+RUN pip3 install pyserial ha_mqtt_discoverable
+
+# Python 3 HTTP Server serves the current working dir
+# So let's set it to our add-on persistent data directory.
+WORKDIR /data
+
+# Copy data for add-on
+COPY run.sh /
+RUN chmod a+x /run.sh
+COPY ./ /pylontech/
+RUN chmod a+x /pylontech/examples/mqtt/PylontechMqtt.py
+
+RUN pip3 install /pylontech
+
+CMD [ "/run.sh" ]
---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/with-contenv bashio
+
+set -e
+
+CONFIG="/etc/dhcpd.conf"
+LEASES="/data/dhcpd.lease"
+
+bashio::log.info "Starting PylontechMqtt.py..."
+
+PYLON=$(bashio::config 'pylon')
+BAUD=$(bashio::config 'baud')
+PACKS=$(bashio::config 'packs')
+MQTT_IP=$(bashio::config 'mqtt_ip')
+MQTT_PORT=$(bashio::config 'mqtt_port')
+MQTT_USER=$(bashio::config 'username')
+MQTT_PASS=$(bashio::config 'password')
+
+
+cd /pylontech/examples/mqtt/
+python3 PylontechMqtt.py --user $MQTT_USER --pass $MQTT_PASS --mqtt $MQTT_IP --port $MQTT_PORT --pylon $PYLON --packs $PACKS --baud $BAUD
