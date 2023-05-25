1ccbcad81008cfb236a2d65d3f41f4df20124877
Daniel Schramm <daniel.schramm@gmx.de>
2023-05-25 21:45:57 +0200

    Device Names changed to remove name collisions.

--- examples/mqtt/MqttStackDevice.py
+++ examples/mqtt/MqttStackDevice.py
@@ -15,38 +15,39 @@ from ha_mqtt_discoverable.sensors import BinarySensor, BinarySensorInfo, Sensor,
 class MqttStackDevice:
 
     def _create_calc_sensors(self):
+        name_base = "PylontechStack_"
         calcSensors = {}
         calcSensors['Power'] = Sensor(
             Settings(mqtt=self.mqtt_settings,
-                     entity=SensorInfo(name="Power", unit_of_measurement="W", device_class="power",
+                     entity=SensorInfo(name=name_base + "Power", unit_of_measurement="W", device_class="power",
                                        unique_id="Pylon_Power_1", device=self.device_info))
         )
         calcSensors['ChargePower_W'] = Sensor(
             Settings(mqtt=self.mqtt_settings,
-                     entity=SensorInfo(name="ChargePower", unit_of_measurement="W", device_class="power",
+                     entity=SensorInfo(name=name_base + "ChargePower", unit_of_measurement="W", device_class="power",
                                        unique_id="Pylon_ChargePower_1", device=self.device_info))
         )
         calcSensors['DischargePower_W'] = Sensor(
             Settings(mqtt=self.mqtt_settings,
-                     entity=SensorInfo(name="DischargePower", unit_of_measurement="W", device_class="power",
+                     entity=SensorInfo(name=name_base + "DischargePower", unit_of_measurement="W", device_class="power",
                                        unique_id="Pylon_DischargePower_1", device=self.device_info))
         )
         calcSensors['RemainCapacity_Ah'] = Sensor(
             Settings(mqtt=self.mqtt_settings,
-                     entity=SensorInfo(name="RemainCapacity", unit_of_measurement="Ah",
+                     entity=SensorInfo(name=name_base + "RemainCapacity", unit_of_measurement="Ah",
                                        device_class="energy_storage", unique_id="Pylon_RemainCapacity_1",
                                        device=self.device_info))
         )
         calcSensors['TotalCapacity_Ah'] = Sensor(
             Settings(mqtt=self.mqtt_settings,
-                     entity=SensorInfo(name="TotalCapacity", unit_of_measurement="Ah",
+                     entity=SensorInfo(name=name_base + "TotalCapacity", unit_of_measurement="Ah",
                                        device_class="energy_storage", unique_id="Pylon_TotalCapacity_1",
                                        device=self.device_info))
         )
 
         calcSensors['Remain_Percent'] = Sensor(
             Settings(mqtt=self.mqtt_settings,
-                     entity=SensorInfo(name="Remain_Percent", unit_of_measurement="%", device_class="battery",
+                     entity=SensorInfo(name=name_base + "Remain_Percent", unit_of_measurement="%", device_class="battery",
                                        unique_id="Pylon_Remain_Percent_1", device=self.device_info))
         )
 
@@ -65,26 +66,3 @@ class MqttStackDevice:
             if e in self.haSensors['Calculated']:
                 print("set: ", e)
                 self.haSensors['Calculated'][e].set_state(newData['Calculated'][e])
-
-"""
-System parameter
-
-{'ADR': 2,
- 'ChargeLowerLimitCurrent': 1817.9,
- 'ChargeLowerLimitTemperature': -195.3,
- 'ChargeUpperLimitTemperature': -681.4,
- 'DischargeLowerLimitCurrent': 1842.8,
- 'DischargeLowerLimitTemperature': -195.3,
- 'DischargeUpperLimitTemperature': -476.6,
- 'ID': 70,
- 'LENGTH': 50,
- 'LowerLimitOfTotalVoltage': 61.619,
- 'PAYLOAD': b'100E420BEA0AF00D030A4703E8D2F0B3B0A7F80D030A47FC18',
- 'RTN': 0,
- 'UnderVoltageOfTotalVoltage': 45.223,
- 'UnitCellHighVoltageThreshold': -5.622,
- 'UnitCellLowVoltageThreshold': 16.907,
- 'UnitCellVoltage': 4.11,
- 'UpperLimitOfTotalVoltage': 59.602,
- 'VER': 32} 
-"""
--- run.sh
+++ run.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/with-contenv bashio
-
-set -e
-
-CONFIG="/etc/dhcpd.conf"
-LEASES="/data/dhcpd.lease"
-
 bashio::log.info "Starting PylontechMqtt.py..."
 
 PYLON=$(bashio::config 'pylon')
@@ -14,7 +8,6 @@ MQTT_IP=$(bashio::config 'mqtt_ip')
 MQTT_PORT=$(bashio::config 'mqtt_port')
 MQTT_USER=$(bashio::config 'username')
 MQTT_PASS=$(bashio::config 'password')
-
 
 cd /pylontech/examples/mqtt/
 python3 PylontechMqtt.py --user $MQTT_USER --pass $MQTT_PASS --mqtt $MQTT_IP --port $MQTT_PORT --pylon $PYLON --packs $PACKS --baud $BAUD
